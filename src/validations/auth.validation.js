77b7552c56d7db1e4dfb4d016562b645b2b2eb35
Mervyn Lee <mervynlee1994@gmail.com>
2021-05-09 01:57:15 +0800

    Remove name field validation

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -5,7 +5,6 @@ const register = {
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().custom(password),
-    name: Joi.string().required(),
   }),
 };
 
